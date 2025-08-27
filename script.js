dda058c55a8f8985a3137b55dea2cf89d1b6c8d2
arnV17 <arnavog@gmail.com>
2025-08-27 18:33:31 +0530

    updates

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 document.getElementById("contactForm").addEventListener("submit", (e) => {
+  if (e.target.name.value === "" || e.target.email.value === ""  || e.target.message.value === "") {
+    alert("Please fill in all fields");
+  }
+  else {
+    alert("Form submitted!");
+  }
     
-  alert("Form submitted!");
 });
 
